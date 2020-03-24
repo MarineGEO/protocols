50e40a99aa8b6f2154252a10ca8c7c03b97280ac
jaxinewolfe <wolfe.ja@husky.neu.edu>
2020-03-24 13:41:52 -0400

    transferring protocol generating workflow to github

---
+++ program-script.R
@@ -0,0 +1,18 @@
+## This will be the program script which executes all the subdirectory generate-protocol.Rmd
+
+# extract file paths to all the protocol-generating .Rmd files within the Modules dir
+
+protocol_paths <- list.files(pattern = "*.Rmd", 
+                             recursive = TRUE, 
+                             full.names = TRUE)
+
+for (i in 1:length(protocol_paths)) {
+  # execute the script and generate the protocols within their appropriate subdirectories
+  rmarkdown::render(input = protocol_paths[i],
+                    # output_file = "something", # only specify to rename the output
+                    output_dir = "protocols-final")
+}
+
+# remove temporary log files
+do.call(file.remove, list(list.files(pattern = "*.log", 
+                                     full.names = TRUE)))
