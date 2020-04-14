1703d946a9b9624554789869c0435279036268b6
jaxinewolfe <wolfe.ja@husky.neu.edu>
2020-04-14 13:46:34 -0400

    updates to logos, protocol generation script, and visual census

--- program-script.R
+++ program-script.R
@@ -2,22 +2,33 @@
 
 # extract file paths to all the protocol-generating .Rmd files within the Modules dir
 
-# generate_protocols <- function(protocol = NULL){
-#   if a protocol is given, render specified protocol
-#   else generate all protocols
-# }
+generate_protocols <- function(protocol = NULL){
 
-protocol_paths <- list.files(pattern = "*.Rmd", 
-                             recursive = TRUE, 
-                             full.names = TRUE)
+  protocol_paths <- list.files(pattern = "*.Rmd", 
+                               recursive = TRUE, 
+                               full.names = TRUE)
+  
+  # if a protocol is given, render specified protocol
+  if(is.null(protocol)==FALSE){
+    path <- protocol_paths[which(grepl(protocol, protocol_paths))]
+    rmarkdown::render(input = path,
+                      output_dir = "protocols-final")
+  }else{
+    #   else generate all protocols
+    for (i in 1:length(protocol_paths)) {
+      # execute the script and generate the protocols within their appropriate subdirectories
+      rmarkdown::render(input = protocol_paths[i],
+                        # output_file = "something", # only specify to rename the output
+                        output_dir = "protocols-final")
+    }
+    }
 
-for (i in 1:length(protocol_paths)) {
-  # execute the script and generate the protocols within their appropriate subdirectories
-  rmarkdown::render(input = protocol_paths[i],
-                    # output_file = "something", # only specify to rename the output
-                    output_dir = "protocols-final")
+  # remove temporary log files
+  do.call(file.remove, list(list.files(pattern = "*.log", 
+                                       full.names = TRUE)))
 }
 
-# remove temporary log files
-do.call(file.remove, list(list.files(pattern = "*.log", 
-                                     full.names = TRUE)))
+# test function
+# generate_protocols("water")
+
+
