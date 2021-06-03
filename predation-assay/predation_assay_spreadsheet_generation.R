2ec7043108b707b855e0fd0bc61cfacde2e90bb0
mlonneman <mlonneman@gmail.com>
2021-06-03 11:37:33 -0400

    create data entry spreadsheets for viz census, predation, ulva, viz census

---
+++ predation-assay/predation_assay_spreadsheet_generation.R
@@ -0,0 +1,15 @@
+# Create sampling_event protocol from schema
+
+library(openxlsx)
+library(tidyverse)
+
+predation_assay_schema <- read_csv("./predation-assay/predation_assay_schema.csv")
+
+doi <- paste0("DOI: ", "10.25573/serc.14717802.v1")
+source("./scripts/spreadsheet_generation_functions.R")
+
+# First arg: name of schema object
+# Second arg: name of protocol in filepath of repository
+# MAKE SURE "final_spreadsheets" DIRECTORY EXISTS
+
+generateSpreadsheets(predation_assay_schema, "predation-assay")
---
+++ visual-census/visual_census_spreadsheet_generation.R
@@ -0,0 +1,15 @@
+# Create sampling_event protocol from schema
+
+library(openxlsx)
+library(tidyverse)
+
+visual_census_schema <- read_csv("./visual-census/visual_census_schema.csv")
+
+doi <- paste0("DOI: ", "10.25573/serc.14717796.v1")
+source("./scripts/spreadsheet_generation_functions.R")
+
+# First arg: name of schema object
+# Second arg: name of protocol in filepath of repository
+# MAKE SURE "final_spreadsheets" DIRECTORY EXISTS
+
+generateSpreadsheets(visual_census_schema, "visual-census")
