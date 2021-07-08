057d84f9d53001bedbad560297559ec4a857c1f9
mlonneman <mlonneman@gmail.com>
2021-07-08 16:43:17 -0400

    add sediment organic matter materials

---
+++ sediment-organic-matter/sediment_organic_matter_spreadsheet_generation.R
@@ -0,0 +1,17 @@
+# Create sediment organic matter data entry spreadsheet from schema
+# Requires R < v4.0
+
+library(openxlsx)
+library(tidyverse)
+
+sedorg_schema <- read_csv("./sediment-organic-matter/sediment-organic-matter_schema.csv")
+
+doi <- paste0("DOI: ", "10.25573/serc.14925111.v1")
+source("./scripts/spreadsheet_generation_functions.R")
+
+# First arg: name of schema object
+# Second arg: name of protocol in filepath of repository
+# MAKE SURE "final_spreadsheets" DIRECTORY EXISTS
+
+generateSpreadsheets(sedorg_schema, "sediment-organic-matter")
+
