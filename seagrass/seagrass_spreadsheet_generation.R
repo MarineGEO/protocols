6b7537e6af5a719baa723fcdcbe14b3b3f8b6a14
mlonneman <mlonneman@gmail.com>
2021-07-09 10:58:51 -0400

    update seagrass protocols

---
+++ seagrass/seagrass_spreadsheet_generation.R
@@ -0,0 +1,15 @@
+# Create seagrass data entry spreadsheets from schema
+
+library(openxlsx)
+library(tidyverse)
+
+seagrass_schema <- read_csv("./seagrass/seagrass_schema.csv")
+
+doi <- paste0("DOI: ", "10.25573/serc.14925114.v1")
+source("./scripts/spreadsheet_generation_functions.R")
+
+# First arg: name of schema object
+# Second arg: name of protocol in filepath of repository
+# MAKE SURE "final_spreadsheets" DIRECTORY EXISTS
+
+generateSpreadsheets(seagrass_schema, "seagrass")
